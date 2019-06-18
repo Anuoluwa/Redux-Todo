2f20d086b92a70da83485a1a42bffe19c2eebb13
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-18 16:21:34 +0100

    return all todos with Redux

--- todo/src/components/TodoComponents/TodoList.js
+++ todo/src/components/TodoComponents/TodoList.js
@@ -1,19 +1,56 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Todo from './Todo';
-function TodoList (props) {
-          return (
-              <div>
-                  {
-                      props.todo.map((todoObj) => {
-                          return  (
-                              <Todo key={todoObj.id} todo={todoObj} markComplete={props.markComplete}/>
-                              )
-                      })
-                  }
-              </div>
-          )
+
+
+export class TodoList extends Component {
+    render() {
+        const todos = this.props.todos || [];
+        return (
+            <div>
+                {
+                    todos.map(todo => (
+                        <Todo 
+                            key={todo.id}
+                            todo={todo}
+                        />
+                    ))
+                }
+            </div>
+        )
+    }
 }
 
-export default TodoList;
+
+
+
+
+function mapStateToProps(state) {
+    return {
+        todos: state.todos,
+    }
+}
+export default connect(
+    mapStateToProps 
+)(TodoList);
+
+
+
+
+
+
+// function TodoList (props) {
+//           return (
+//               <div>
+//                   {
+//                       props.todo.map((todoObj) => {
+//                           return  (
+//                               <Todo key={todoObj.id} todo={todoObj} markComplete={props.markComplete}/>
+//                               )
+//                       })
+//                   }
+//               </div>
+//           )
+// }
+
+// export default TodoList;
